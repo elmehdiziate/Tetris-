010fb518b871918cbd6435beffb45187a6cf944b
elmehdiziate <mziate23@gmail.com>
2023-05-25 06:30:55 +0100

    score

--- Tetris.py
+++ Tetris.py
@@ -8,8 +8,17 @@ class Content:
         self.app = app
         self.font = ft.Font(FONT_PATH)
     def draw(self):
-        self.font.render_to(self.app.screen, (WIN_W*0.6 , WIN_H*0.02), 
+        self.font.render_to(self.app.screen, (WIN_W*0.595 , WIN_H*0.02), 
                             text="TETRIS-", fgcolor='white', size = TILE_SIZE *1.75, 
+                            bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W*0.675 , WIN_H*0.25), 
+                            text="Next", fgcolor='white', size = TILE_SIZE *1.5, 
+                            bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W*0.63 , WIN_H*0.65), 
+                            text="Score", fgcolor='white', size = TILE_SIZE *1.5, 
+                            bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W*0.775 , WIN_H*0.8), 
+                            text=f'{self.app.tetris.score}', fgcolor='white', size = TILE_SIZE *1.4, 
                             bgcolor='black')
 class Tetris:
     def __init__(self, app):
@@ -19,10 +28,19 @@ class Tetris:
         self.tetromino = Tetromino(self)
         self.speed_up = False
         self.next_tetromino = Tetromino(self, current = False)
+        self.score = 0
+        self.completed_lines = 0
+        self.points_per_line = {0:0,1:100, 2:300, 3:700, 4:1500}
+    
+    def get_score(self):
+        self.score += self.points_per_line[self.completed_lines]
+        self.completed_lines = 0
+        
     def game_over(self):
         if self.tetromino.blocks[0].pos.y == INIT_POS_OFFSET[1]:
             pg.time.wait(300)
             return True
+
     def check_lines(self):
         row = FIELD_H - 1
         for y in range(FIELD_H - 1, -1, -1):
@@ -36,6 +54,8 @@ class Tetris:
                 for x in range(FIELD_W):
                     self.field_array[row][x].alive = False
                     self.field_array[row][x] = 0
+                self.completed_lines += 1
+ 
     def put_blocks_in_array(self):
         for block in self.tetromino.blocks:
             x,y = int(block.pos.x), int(block.pos.y)
@@ -72,12 +92,14 @@ class Tetris:
             for y in range(FIELD_H):
                 pg.draw.rect(self.app.screen, "black",
                              (x* TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+
     def update(self):
         trigger = [self.app.anime_trigger, self.app.fast_trigger][self.speed_up]
         if trigger == True:
             self.check_lines()
             self.tetromino.update()
             self.check_landing()
+            self.get_score()
         self.sprite_group.update()
 
     def draw(self):
